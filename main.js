fe9d69791992c6a70e2d63d1ac385256a04dfff0
adityamishra-2008 <86653579+adityamishra-2008@users.noreply.github.com>
2022-06-01 09:56:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload(){
+}
+
+function setup() {
+    canvas = createCanvas(500,400);
+    canvas.position(350,250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+tint_color="";
+
+function draw() {
+    image(video,100,80,300,250);
+    tint(tint_color);
+   
+
+    stroke(168, 10, 10);
+    fill(168, 10, 10);
+    
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+
+
+    stroke(50, 168, 82);
+    fill(50, 168, 82);
+
+    rect(63, 27, 375, 25);
+    rect(63, 347, 375, 25);
+    rect(27, 63, 25, 275);
+    rect(448, 63, 25, 275);
+}
+
+
+
+
+
+    
+    function apply_filter(){
+        tint_color=document.getElementById("color").value;
+        document.getElementById("btn-warning").style.backgroundColor="rgb(97, 99, 102)";
+    }
+
+
+function take_snapshot(){
+    save("frame.png");
+}
+    
+
+   
